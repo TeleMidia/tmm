ee4a31d06675413eae73947069812930cbb50811
fnagato <fnagato@telemidia.puc-rio.br>
2013-10-03 15:16:28 -0300

    Adding missing file (Pait.cpp).

---
+++ src/project/si/PAit.cpp
@@ -0,0 +1,41 @@
+/*
+ * PAit.cpp
+ *
+ *  Created on: 03/10/2013
+ *      Author: Felippe Nagato
+ */
+
+#include "project/si/PAit.h"
+
+namespace br {
+namespace pucrio {
+namespace telemidia {
+namespace tool {
+
+PAit::PAit() {
+	projectType = PT_AIT;
+	layer = 0x02; //HIERARCHY_B
+	carouselProj = NULL;
+}
+
+PAit::~PAit() {
+
+}
+
+void PAit::setCarouselProj(ProjectInfo* proj) {
+	carouselProj = proj;
+}
+
+ProjectInfo* PAit::getCarouselProj() {
+	return carouselProj;
+}
+
+}
+}
+}
+}
+
+
+
+
+
