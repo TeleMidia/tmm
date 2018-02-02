dc8ab25aac81f18624ccf036abb88d9504e3e2e8
Rafael Diniz <rafael@riseup.net>
2018-02-02 12:02:51 -0200

    Improved DtPlay script.

--- docs/DtPlay.sh
+++ docs/DtPlay.sh
@@ -2,6 +2,8 @@
 
 if ! [ -x "$(command -v DtPlay)" ]; then
   echo "Error: DtPlay is not installed."
+  echo "Install using Linux SDK and DtPlay packages from DekTec website."
+  echo "https://www.dektec.com/downloads/SDK/"
   exit 1
 fi
 
@@ -10,6 +12,5 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-
-# canal 20
-DtPlay $1 -mt ISDBT -t 215 -mf 509.14 -l 0
+# canal 14
+DtPlay $1 -mt ISDBT -t 215 -mf 473.14 -l 0
